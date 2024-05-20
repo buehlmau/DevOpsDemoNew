0532c63bc4e323bfa1226bbc8e026b3eab2bdfb3
buehlmau <buehlmau@students.zhaw.ch>
2024-05-20 13:53:24 +0200

    CodeSmell im CalculatorFizzTest.java bereinigt

--- backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/CalculatorFizzTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/fizzbuzz/CalculatorFizzTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
-public class CalculatorFizzTest {
+class CalculatorFizzTest {
     @Test
     public void testMultiplyFizz() {
         CalculatorFizz fizzBuzz = new CalculatorFizz();
